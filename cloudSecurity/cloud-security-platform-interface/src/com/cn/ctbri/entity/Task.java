549ca2a0c720dda212565ead6e3c416271459564
user <user@3b805a0e-90b7-4baf-9172-f1dffca5d966>
2015-01-23 09:07:34 +0000

    任务实体

---
+++ cloudSecurity/cloud-security-platform-interface/src/com/cn/ctbri/entity/Task.java
@@ -0,0 +1,31 @@
+package com.cn.ctbri.entity;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class Task implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 任务id
+	 */
+	private Long teskId;
+	
+	/**
+	 * 订单资产详情Id
+	 */
+	private Long order_asset_Id;
+	
+	/**
+	 * 任务执行时间
+	 */
+	private Date execute_time;
+	
+	
+	
+	/**
+	 * 备注
+	 */
+	private String remarks;
+}
