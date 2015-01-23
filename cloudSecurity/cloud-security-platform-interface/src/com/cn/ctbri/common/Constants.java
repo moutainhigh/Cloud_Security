0dd69246741168bb5cd661ef581a71f569be1826
user <user@3b805a0e-90b7-4baf-9172-f1dffca5d966>
2015-01-23 08:01:02 +0000

    常量表

---
+++ cloudSecurity/cloud-security-platform-interface/src/com/cn/ctbri/common/Constants.java
@@ -0,0 +1,26 @@
+package com.cn.ctbri.common;
+/**
+ * 常量表
+ * @author googe
+ *
+ */
+		
+public class Constants {
+	/**
+	 * 任务状态<br>
+	 * start:已开始
+	 */
+	public static final String START = "start"; 
+	
+	/**
+	 * 任务状态<br>
+	 * finish:已结束
+	 */
+	public static final String FINISH = "finish"; 
+	
+	/**
+	 * 任务状态<br>
+	 * running :执行中
+	 */
+	public static final String RUNNING = "running "; 
+}
