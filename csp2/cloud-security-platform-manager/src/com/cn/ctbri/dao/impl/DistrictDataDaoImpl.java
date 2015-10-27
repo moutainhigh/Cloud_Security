c1ca76b1ef1f30ab2e9929c4d69f84d17fcf26ac
user <user@3b805a0e-90b7-4baf-9172-f1dffca5d966>
2015-10-27 08:31:40 +0000

    省市联动

--- csp2/cloud-security-platform-manager/src/com/cn/ctbri/dao/impl/DistrictDataDaoImpl.java
+++ csp2/cloud-security-platform-manager/src/com/cn/ctbri/dao/impl/DistrictDataDaoImpl.java
@@ -14,6 +14,7 @@ import com.cn.ctbri.dao.DaoCommon;
 import com.cn.ctbri.dao.DistrictDataDao;
 import com.cn.ctbri.dao.TaskDao;
 import com.cn.ctbri.entity.Alarm;
+import com.cn.ctbri.entity.City;
 import com.cn.ctbri.entity.District;
 import com.cn.ctbri.entity.Task;
 /**
@@ -30,6 +31,7 @@ public class DistrictDataDaoImpl extends DaoCommon implements DistrictDataDao {
 	 * 功        能： DistrictDataMapper命名空间
 	 */
 	private String ns = "com.cn.ctbri.entity.DistrictDataMapper.";
+	private String nsCity = "com.cn.ctbri.entity.CityMapper.";
 
     public List<District> getDistrictByAll(Map<String, Object> paramMap) {
         return getSqlSession().selectList(ns + "findDistrictList",paramMap);
@@ -70,7 +72,23 @@ public class DistrictDataDaoImpl extends DaoCommon implements DistrictDataDao {
 
 	public Alarm getServiceAlarmByMonth(Map<String, Object> paramMap) {
 		return getSqlSession().selectOne(ns + "getServiceAlarmByMonth", paramMap);
-	}		
+	}
+
+	/**
+	 * 功能描述：地区查询
+	 * 返回值    ：List<District>
+	 */
+	public List<District> getDistrictList() {
+		return getSqlSession().selectList(ns + "findDistrictListAll");
+	}
+	/**
+	 * 功能描述：城市查询
+	 * 参数描述：paramMap
+	 * 返回值    ：List<District>
+	 */
+	public List<City> getCityListByProv(Map<String, Object> paramMap) {
+		return getSqlSession().selectList(nsCity + "findCityList",paramMap);
+	}	
 	
 	
 }
