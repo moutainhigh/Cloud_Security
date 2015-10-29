291a2958b0f21321305b9a1de97b652fd0d034ab
user <user@3b805a0e-90b7-4baf-9172-f1dffca5d966>
2015-10-29 03:36:12 +0000

    资产用途统计分析

--- csp2/cloud-security-platform-manager/WebRoot/source/scripts/assetJs/backstage.js
+++ csp2/cloud-security-platform-manager/WebRoot/source/scripts/assetJs/backstage.js
@@ -1,7 +1,13 @@
 $(function(){
-	//选项卡
+	$('.tabList').children().eq(tablList).addClass('active').siblings('li').removeClass('active');
+	$('.tabCont .tabItem:eq('+tablList+')').show().siblings().hide();
+    $('.anlist:eq('+tablList+')').children().eq(anList).addClass('active').siblings('li').removeClass('active');
+    //alert($('.analyse_tabCont:eq('+tablList+')').children().eq(anList).html());
+    $('.analyse_tabCont:eq('+tablList+')').children().eq(anList).show().siblings().hide();
+    //选项卡
 $('.tabList li').click(function(){
 	var index=$(this).index();
+	//alert("tabList"+index);
 	$(this).addClass('active').siblings('li').removeClass('active');
 	$('.tabCont .tabItem:eq('+index+')').show().siblings().hide();
 });
@@ -10,6 +16,7 @@ $('.tabList li').click(function(){
 	
 $('.anlist li').click(function(){
 	var index=$(this).index();
+	//alert("anlist"+index);
 	$(this).addClass('active').siblings('li').removeClass('active');
 	
 	$(this).parent('.anlist').siblings('.analyse_tabCont').children('.analyse_tabItem:eq('+index+')').show().siblings().hide();
