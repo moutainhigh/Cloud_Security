cf9ea8533d23bd913f0f5f9ae6545d0075787dca
user <user@3b805a0e-90b7-4baf-9172-f1dffca5d966>
2015-11-02 02:48:15 +0000

    资产服务统计分析

--- csp2/cloud-security-platform-manager/WebRoot/source/scripts/assetJs/backstage.js
+++ csp2/cloud-security-platform-manager/WebRoot/source/scripts/assetJs/backstage.js
@@ -1,9 +1,5 @@
 $(function(){
-	$('.tabList').children().eq(tablList).addClass('active').siblings('li').removeClass('active');
-	$('.tabCont .tabItem:eq('+tablList+')').show().siblings().hide();
-    $('.anlist:eq('+tablList+')').children().eq(anList).addClass('active').siblings('li').removeClass('active');
-    //alert($('.analyse_tabCont:eq('+tablList+')').children().eq(anList).html());
-    $('.analyse_tabCont:eq('+tablList+')').children().eq(anList).show().siblings().hide();
+	
     //选项卡
 $('.tabList li').click(function(){
 	var index=$(this).index();
@@ -42,5 +38,85 @@ $('.user_select').change(function(){
 		$(this).parents('.user_form').siblings('.tableBox').find('.tableUser').hide();		
 	}
 })
-	
+
 })
+
+//添加省下拉框
+function putOption(obj){
+	var list = obj;
+	for (var i = 0; i < list.length; i++){
+		var tmp=list[i].id;
+		if(prov!=''){
+			if(tmp==prov){
+			    $("#prov").append( "<option selected='selected' value="+list[i].id+">"+list[i].name+"</option>" );
+			}else{
+				$("#prov").append( "<option value="+list[i].id+">"+list[i].name+"</option>" );
+			}
+		}else{
+			$("#prov").append( "<option value="+list[i].id+">"+list[i].name+"</option>" );
+		}
+		
+	}
+}
+
+function getCitys(provId){
+	//alert("ddddddddddddd");
+	//查询省对应的市  
+	 $.ajax({
+		 	data: {"provId":provId},
+	        type: "POST",
+	        cache: false,
+	        dataType: "json",
+	        url: "getCityList.html", 
+	        success: function(obj){
+	        	putCityOption(obj);
+	        	
+	     	}
+		});
+}
+
+//添加市下拉框
+function putCityOption(obj){
+	//清空城市下拉框
+	$("#city").empty();
+	var list = obj;
+	if(list != null && list.length > 0){
+		//alert(city);
+		for (var i = 0; i < list.length; i++){
+			var tmp=list[i].id;
+            if(city!=''){
+				if(tmp==city){
+					$("#city").append( "<option selected='selected' value="+list[i].id+">"+list[i].name+"</option>" );	
+				}else{
+					$("#city").append( "<option value="+list[i].id+">"+list[i].name+"</option>" );	
+				}	
+			}else{
+			   $("#city").append( "<option value="+list[i].id+">"+list[i].name+"</option>" );	
+		    }		
+		}
+		$("#city").removeAttr("disabled");
+	}
+}
+
+$(document).ready(function(){
+	$('.tabList').children().eq(tablList).addClass('active').siblings('li').removeClass('active');
+	$('.tabCont .tabItem:eq('+tablList+')').show().siblings().hide();
+    $('.anlist:eq('+tablList+')').children().eq(anList).addClass('active').siblings('li').removeClass('active');
+   // alert($('.analyse_tabCont:eq('+tablList+')').children().eq(anList).html());
+    $('.analyse_tabCont:eq('+tablList+')').children().eq(anList).show().siblings().hide();
+  //查询省
+	 $.ajax({
+	        type: "POST",
+	        cache: false,
+	        dataType: "json",
+	        url: "getDistrictListAll.html", 
+	        success: function(obj){
+	        	putOption(obj);
+	     	}
+		});
+	
+    $("#assetType1").val(assetUserType);
+     if(city!=''){
+       getCitys(prov);
+     }
+});
