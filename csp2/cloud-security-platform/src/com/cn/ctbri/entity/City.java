8681d06138cb9746ea31922750587138054d2217
user <user@3b805a0e-90b7-4baf-9172-f1dffca5d966>
2015-11-04 06:18:49 +0000

    修改city数据库相关

--- csp2/cloud-security-platform/src/com/cn/ctbri/entity/City.java
+++ csp2/cloud-security-platform/src/com/cn/ctbri/entity/City.java
@@ -2,24 +2,13 @@ package com.cn.ctbri.entity;
 
 public class City {
 	private int id;
-	private String code; //城市编码
 	private String name; //城市中文名称
-	private int type;	 //城市类别：南方分为1、2、3、4类；北方分为1、2类
 	private int districtId;  //所属省ID
-	private String provCode; //所属省份编码
-	private String provName; //省份名称
-	private String area;	 //城市所属区域：0南方、1北方
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
-	}
-	public String getCode() {
-		return code;
-	}
-	public void setCode(String code) {
-		this.code = code;
 	}
 	public String getName() {
 		return name;
@@ -27,36 +16,12 @@ public class City {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public int getType() {
-		return type;
-	}
-	public void setType(int type) {
-		this.type = type;
-	}
 
 	public int getDistrictId() {
 		return districtId;
 	}
 	public void setDistrictId(int districtId) {
 		this.districtId = districtId;
-	}
-	public String getProvCode() {
-		return provCode;
-	}
-	public void setProvCode(String provCode) {
-		this.provCode = provCode;
-	}
-	public String getProvName() {
-		return provName;
-	}
-	public void setProvName(String provName) {
-		this.provName = provName;
-	}
-	public String getArea() {
-		return area;
-	}
-	public void setArea(String area) {
-		this.area = area;
 	}
 	
 }
