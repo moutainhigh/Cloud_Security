96a8e0ede4b4b6c2b0a0203139e82e6dc8bb358c
user <user@3b805a0e-90b7-4baf-9172-f1dffca5d966>
2016-12-06 08:29:49 +0000

    主干2

--- exam/src/exam/aaa.java
+++ exam/src/exam/aaa.java
@@ -18,7 +18,7 @@ public class aaa {
 	    return sb.toString();  
 	} 
 	public static void main(String[] args) {
-		System.out.println("主干");
+		System.out.println("主干2");
 		System.out.println("Hello World");
 	}
 }
