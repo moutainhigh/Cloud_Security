23ab56fe841a35be129899e116a91de39f7c9728
user <user@3b805a0e-90b7-4baf-9172-f1dffca5d966>
2017-03-07 03:08:27 +0000

    购物车

--- csp2/cloud-security-platform-jg/WebRoot/source/scripts/order/sysServDetail.js
+++ csp2/cloud-security-platform-jg/WebRoot/source/scripts/order/sysServDetail.js
@@ -68,12 +68,63 @@ $(function(){
     });
   });
 
+//点击“添加购物车”
+  $("#addSysCar").click(function(){
+  	alert("abc");
+  	var shopCarNum = Number($("#shopCarNum").html());
+	if (shopCarNum >= 99) {
+		alert("购物车中商品数量已达上限，请先清理购物车！");
+		return;
+	}
+	var createDate = getCreateDate();
+	var orderType = "1";
+	var duration = $("#duration").val();
+	var indexPage = $("#indexPage").val();//标记从首页进入自助下单流程
+	var serviceId = $("#serviceIdHidden").val();
+	var scanType = null;
+    if(serviceId==7)
+  	  scanType=$("#ipNum").val();
+    else if(serviceId==8)
+  	  scanType=$("#nodeNum").val();
+    
+	$.ajax({ type: "POST",
+	     async: false, 
+	     url: "shoppingCar.html",
+	     data: {"orderType":orderType,
+			   	"duration": duration,
+			   	"scanType":scanType,
+			   	"serviceId":serviceId}, 
+	     dataType: "json", 
+	     success: function(data) {
+			   		if(data.error){
+			   			alert("参数值数据异常!");
+			   			//window.location.href="index.html";
+			   		}
+			   		 if(data.sucess){
+			   			 alert("添加购物车成功!");
+			   			 buyService(serviceId);
+			   			// window.location.href="selfHelpOrderInit.html?serviceId="+serviceId+"&indexPage="+indexPage;
+			   		 }
+			   			
+			   	
+	    	 }, 
+	     error: function(data){ 
+	    	 if (data.responseText.indexOf("<!DOCTYPE html>") >= 0) { 
+	    		 //window.location.href = "loginUI.html"; 
+	    		 alert("请求失败");
+	    	 } 
+	    	 else { 
+	    		 //window.location.href = "loginUI.html"; 
+	    		 alert("请求失败");
+	    		 } } 
+	});
+  });
   
   //结算页提交订单
   $("#settlementSys").click(function(){
   	var createDate = getCreateDate();
-  	var orderType = $('#orderType').val();
-  	var orderDetailId = $("#orderDetailId").val();	//muji
+  	var orderType =  $('#orderType').val();
+  	var orderDetailId = $("#orderDetailId").val();
   	var userName =  $(".test_name").text();
     var userAdd = $(".test_add").text();
     var mobile =  $(".test_iphone").text();
@@ -94,10 +145,7 @@ $(function(){
    				alert("参数值数据异常!!");
    				window.location.href = "index.html";
   		     	    return;
-   			}else if(data.assetsStatus == true){
-  					alert("订单资产没有验证,请重新下单!");
-  		     	    return;
-  				}else if(data.timeCompare == false){
+   			}else if(data.timeCompare == false){
   					alert("订单无效,请重新下单!");
   		     		return;
   				}else if(data.orderStatus == false){
@@ -125,14 +173,18 @@ $(function(){
   	    }, 
   	    error: function(data){ 
   	    	if (data.responseText.indexOf("<!DOCTYPE html>") >= 0) { 
-	    		 window.location.href = "loginUI.html"; } 
-	    	 else { window.location.href = "loginUI.html"; }
+	    		// window.location.href = "loginUI.html"; 
+	    		 } 
+	    	 else { 
+	    		 //window.location.href = "loginUI.html"; 
+	    		 }
   	    } 
   	});
   });
   
 });
 
+//金山商品详情页中服务期限与管理终端数的联动对应
 function changeDur(){
 	var duration = $("#duration").val();
 	$("#nodeNum").empty();
@@ -192,3 +244,20 @@ function changePrice(){
       } 
   });
 }
+
+//跳转到购物车页面
+function showShopCar(){
+		$.ajax({ type: "POST",
+		     async: false, 
+		     url: "getSession.html", 
+		     dataType: "json", 
+		     success: function(data) {
+		    	 window.location.href="showShopCar.html";
+		    	 }, 
+		     error: function(data){ 
+		    	 if (data.responseText.indexOf("<!DOCTYPE html>") >= 0) { 
+		    		 window.location.href = "loginUI.html"; } 
+		    	 else { window.location.href = "loginUI.html"; } } 
+		});
+
+		}
