6e3dc32e2bb8eb01f1b0e742effe284ec091a575
user <user@3b805a0e-90b7-4baf-9172-f1dffca5d966>
2017-04-10 02:28:42 +0000

    加载接口返回值key，value

--- csp2/service-management-system_brunch/src/com/cn/ctbri/listener/CspContextListener.java
+++ csp2/service-management-system_brunch/src/com/cn/ctbri/listener/CspContextListener.java
@@ -1,7 +1,10 @@
 package com.cn.ctbri.listener;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Properties;
 
@@ -10,15 +13,13 @@ import javax.servlet.ServletContextListener;
 
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
-
-import com.cn.ctbri.nio.UdpServerSocket;
 /**
  * 描     述： 服务器监听器
  * 創建人: 于永波
  * 日    期: 2015-3-16
  */
 public class CspContextListener implements ServletContextListener{
-	public static HashMap<String,String> serviceCodeAndMessage = new  HashMap<String,String>();
+	public static HashMap<String,String> allPropertisMap = new  HashMap<String,String>();
 	
 	
 	public void contextDestroyed(ServletContextEvent sce) {
@@ -30,31 +31,37 @@ public class CspContextListener implements ServletContextListener{
 		WebApplicationContext context = WebApplicationContextUtils.getWebApplicationContext(sce.getServletContext());
 //		UdpServerSocket server = context.getBean("serverNetty", UdpServerSocket.class);
 //		server.start();
-		putServiceCodeAndMessageMap();
+		putMapByPropName("codeAndMessage");
+		putMapByPropName("southAPI");
 		   
 	}
-	private void putServiceCodeAndMessageMap(){
-		Properties p = new Properties();
+	private void putMapByPropName(String propName){
+		Properties pros = new Properties();
+		InputStream stream = CspContextListener.class.getClassLoader().getResourceAsStream(propName + ".properties");
+		Reader reader = null;
 		try {
+			reader = new InputStreamReader(stream, "UTF-8");
+			pros.load(reader);
 			
-			p.load(new InputStreamReader(CspContextListener.class.getClassLoader().getResourceAsStream("codeAndMessage.properties"), "UTF-8"));
+			Enumeration en = pros.propertyNames();//得到资源文件中的所有key值  
+	        while (en.hasMoreElements()) {  
+			    String key = (String) en.nextElement();
+			    allPropertisMap.put(key, pros.getProperty(key));
+			}			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}finally{
+			if( reader != null)
+				try {
+					reader.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			
 		}
-		serviceCodeAndMessage.put("successCode", p.getProperty("successCode"));
-		serviceCodeAndMessage.put("successMessage", p.getProperty("successMessage"));
-		serviceCodeAndMessage.put("failCode", p.getProperty("failCode"));
-		serviceCodeAndMessage.put("failMessage", p.getProperty("failMessage"));
-		serviceCodeAndMessage.put("paramErrCode", p.getProperty("paramErrCode"));
-		serviceCodeAndMessage.put("paramErrMessage", p.getProperty("paramErrMessage"));
-		serviceCodeAndMessage.put("tokenInvalidCode", p.getProperty("tokenInvalidCode"));
-		serviceCodeAndMessage.put("tokenInvalidMessage", p.getProperty("tokenInvalidMessage"));
-		serviceCodeAndMessage.put("permissionDeniedCode", p.getProperty("permissionDeniedCode"));
-		serviceCodeAndMessage.put("permissionDeniedMessage", p.getProperty("permissionDeniedMessage"));
-		serviceCodeAndMessage.put("expiredCode", p.getProperty("expiredCode"));
-		serviceCodeAndMessage.put("expiredMessage", p.getProperty("expiredMessage"));
-		serviceCodeAndMessage.put("usedUpCode", p.getProperty("usedUpCode"));
-		serviceCodeAndMessage.put("usedUpMessage", p.getProperty("usedUpMessage"));
+		
+		
 	}
 }
