8dff9d3dae4c49c9c0c574848ecb4f10d6eb1b6e
lilingxiao <lilingxiao@3b805a0e-90b7-4baf-9172-f1dffca5d966>
2017-06-29 01:58:13 +0000

    系统安全帮二级页面广告获取更新

--- csp2/cloud-security-platform/src/com/cn/ctbri/controller/UserController.java
+++ csp2/cloud-security-platform/src/com/cn/ctbri/controller/UserController.java
@@ -2197,8 +2197,8 @@ public class UserController{
         int webSite = selfHelpOrderService.findWebSite();
         //断网次数
         int brokenNetwork = selfHelpOrderService.findBrokenNetwork();
-        //获取安全能力API页面的广告
-        List adList = adService.findAdvertisementByType(2);
+        //获取系统安全帮页面的广告
+        List adList = adService.findAdvertisementByType(3);
         
         System.out.println(servList);
         m.addAttribute("leakNum", leakNum);
