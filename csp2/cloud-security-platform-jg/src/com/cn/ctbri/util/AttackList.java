fe9ca1f3e244ca90a32ae3bdb5f6463fdc644a35
litianb <litianb@3b805a0e-90b7-4baf-9172-f1dffca5d966>
2017-07-20 07:06:21 +0000

    报告中的列表类

---
+++ csp2/cloud-security-platform-jg/src/com/cn/ctbri/util/AttackList.java
@@ -0,0 +1,20 @@
+package com.cn.ctbri.util;
+
+public class AttackList {
+
+	private String sourceIP;
+	public String getSourceIP() {
+		return sourceIP;
+	}
+	public void setSourceIP(String sourceIP) {
+		this.sourceIP = sourceIP;
+	}
+	private int num ;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+}
