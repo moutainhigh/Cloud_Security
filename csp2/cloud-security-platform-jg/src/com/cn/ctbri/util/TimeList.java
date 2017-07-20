0465067b107a16e2653a0e65e2976a6ca0f9edba
litianb <litianb@3b805a0e-90b7-4baf-9172-f1dffca5d966>
2017-07-20 07:06:36 +0000

    报告中的列表类

---
+++ csp2/cloud-security-platform-jg/src/com/cn/ctbri/util/TimeList.java
@@ -0,0 +1,20 @@
+package com.cn.ctbri.util;
+
+public class TimeList {
+
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	private int num ;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+}
