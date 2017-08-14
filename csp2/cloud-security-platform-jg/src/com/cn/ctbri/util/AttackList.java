f4ac17bd4d525f783a953d223917818c4315ca5b
litianb <litianb@3b805a0e-90b7-4baf-9172-f1dffca5d966>
2017-08-14 08:48:07 +0000

    增加国家地区字段

--- csp2/cloud-security-platform-jg/src/com/cn/ctbri/util/AttackList.java
+++ csp2/cloud-security-platform-jg/src/com/cn/ctbri/util/AttackList.java
@@ -3,13 +3,31 @@ package com.cn.ctbri.util;
 public class AttackList {
 
 	private String sourceIP;
+	private String num ;
+	private String subdiv;
+	private String country;
+
+	
+	public String getSubdiv() {
+		return subdiv;
+	}
+	public void setSubdiv(String subdiv) {
+		this.subdiv = subdiv;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
 	public String getSourceIP() {
 		return sourceIP;
 	}
 	public void setSourceIP(String sourceIP) {
 		this.sourceIP = sourceIP;
 	}
-	private String num ;
+	
 	
 	public String getNum() {
 		return num;
@@ -17,4 +35,6 @@ public class AttackList {
 	public void setNum(String num) {
 		this.num = num;
 	}
+	
+	
 }
