913f2bf9025750351175035ec517cd375792f06f
liuhansheng <liuhansheng@3b805a0e-90b7-4baf-9172-f1dffca5d966>
2017-09-29 03:21:55 +0000

    修复插入主键冲突bug

--- csp2/cloud-security-platform-jg/src/com/cn/ctbri/service/impl/IPPositionServiceImpl.java
+++ csp2/cloud-security-platform-jg/src/com/cn/ctbri/service/impl/IPPositionServiceImpl.java
@@ -43,7 +43,10 @@ public class IPPositionServiceImpl implements IIPPositionService{
 	}
 
 	public void saveIPPosition(IPPosition ipPosition) {
-		ipPositionDao.saveAsset(ipPosition);
+		if(ipPosition.getLongitude()!=null&&ipPosition.getLatitude()!=null
+				&&ipPosition.getLongitude().length()>0&&ipPosition.getLatitude().length()>0){
+			ipPositionDao.saveAsset(ipPosition);
+		}
 	}
 
 	public int delete(String  ip) {
